a1c09e1fb934e19781098b2ec4b02cb23a22868f
Mattias Seth <sethm@chalmers.se>
2023-12-04 17:45:31 +0100

    Fixed code base for project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <MPU6050.h>
+#include <Adafruit_ADXL345_U.h>
 #include <Adafruit_BusIO_Register.h>
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
@@ -8,44 +9,71 @@
 #include <AsyncTCP.h>
 #include <PubSubClient.h> // Include the PubSubClient library
 
+// const int batteryPin = 2;  // Analog pin to measure battery voltage
+
+// void setup() {
+//     Serial.begin(9600);
+// }
+
+// void loop() {
+//     // Read battery voltage
+//     int rawValue = analogRead(batteryPin);
+//     Serial.println(rawValue);
+//     float voltage = (rawValue / 4095.0) * 3.6;  // Assuming a voltage divider is used
+
+//     // Check if voltage is below a threshold (adjust as needed)
+//     if (voltage < 3.8) {
+//         Serial.println("Running on battery power!");
+//         Serial.println(voltage);
+//     } else {
+//         Serial.println("Running on external power or USB." );
+//         Serial.println(voltage);
+//     }
+
+//     delay(5000);  // Delay for 5 seconds before checking again
+// }
+
+
+
+
+
 MPU6050 mpu; // Define the sensor
 const char* ssid = "iPhone (8)";
 const char* password = "NYluft1993";
 
 const char* mqttServer = "broker.hivemq.com";
+// const char* mqttServer = "mqtt.eclipse.org";
 const int mqttPort = 1883; // Default MQTT port
+// const int mqttPort = 8883; // Default MQTT port
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 bool mqttConnected = false; // Maintain MQTT connection state
 
-
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastTime = 0;
-// Timer set to 10 minutes (600000)
-//unsigned long timerDelay = 600000;
-// Set timer to 5 seconds (5000)
-unsigned long timerDelay = 5000;
+
 
 void setup() {
-  Serial.begin(9600);
+
+  // 9600 bits per second, communication betwen the IMU and your computer
+  Serial.begin(9600);;
   // initialize sensor
   Wire.begin();
-  mpu.initialize(); // Initialize the MPU6050
+  // Initialize the MPU6050
+  mpu.initialize(); 
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    // delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
- 
-  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
-
+  
   // Initialize MQTT client
   mqttClient.setServer(mqttServer, mqttPort);
   mqttClient.setKeepAlive(100); // Keep-alive interval in seconds
@@ -57,12 +85,12 @@ void reconnectMQTT() {
     Serial.println("Attempting MQTT connection...");
     if (mqttClient.connect("ESP32Client")) {
       Serial.println("Connected to MQTT broker");
-      mqttClient.subscribe("EEN210"); // Subscribe to MQTT topic(s)
+      mqttClient.subscribe("seth1993"); // Subscribe to MQTT topic(s)
       mqttConnected = true; // Set MQTT connection state to true
     } else {
       Serial.print("Failed to connect to MQTT broker, rc=");
       Serial.println(mqttClient.state());
-      delay(5000);
+      // delay(5000);
     }
   }
 }
@@ -71,11 +99,11 @@ void loop() {
   Serial.println("inside loop");
 
   if (WiFi.status() == WL_CONNECTED) {
-    if (!mqttConnected) {
+    while (!mqttConnected) {
       reconnectMQTT();
     }
 
-    char topic[] = "EEN210!"; // Replace with the MQTT topic you want to publish to
+    char topic[] = "ddwddwdd"; // Replace with the MQTT topic you want to publish to
 
     int16_t ax, ay, az;
     int16_t gx, gy, gz;
@@ -101,7 +129,8 @@ void loop() {
                     ",\"gyroscope_x\":" + String(gyroX, 4) +
                     ",\"gyroscope_y\":" + String(gyroY, 4) +
                     ",\"gyroscope_z\":" + String(gyroZ, 4) + "}";
-
+                    
+    Serial.println(payload);
     // Publish the data to the MQTT topic
     if (mqttClient.publish(topic, payload.c_str())) {
       Serial.println("Message sent successfully");
@@ -114,5 +143,5 @@ void loop() {
   } else {
     Serial.println("error wifi");
   }
-  delay(100);
+  // delay(100);
 }
--- src/server.py
+++ src/server.py
@@ -1,43 +1,146 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
 import paho.mqtt.client as mqtt
-from fastapi.templating import Jinja2Templates
+import json
+import pandas as pd
+from datetime import datetime
+import asyncio
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")  # Create a "templates" folder in your project directory
 
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Fall detection</title>
+         <style>
+            /* Add some CSS for styling the scrollable area */
+            #messages {
+                overflow-y: scroll;
+                height: 90vh;  /* Limit the height of the scrollable area */
+                width: 60vw;  /* Limit the height of the scrollable area */
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Real-time data collection</h1>
+        <button id="closeButton">Stängd</button>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages');
+                var message = document.createElement('li');
+                var content = document.createTextNode(event.data);
+                message.appendChild(content);
+       
+                 // Parse the JSON data
+                var data = JSON.parse(event.data);
+
+                // Check if gyroscope_z is less than -22
+                if (data.acceleration_x > 0.1) {
+                    message.style.color = 'red'; // Set the text color to red
+                }
+                messages.appendChild(message);
+                messages.scrollTop = messages.scrollHeight;
+            };
+                 close = function(){
+                    ws.send("close");
+            }
+             var closeButton = document.getElementById('closeButton');
+            closeButton.addEventListener('click', function() {
+                // Call the close function when the button is clicked
+                close();
+            });
+       
+        </script>
+    </body>
+</html>
+"""
 
 class MQTTHandler:
     def __init__(self, broker_address, mqtt_topic):
         self.broker_address = broker_address
         self.mqtt_topic = mqtt_topic
         self.mqtt_data = None
+        self.columns = ["timestamp", "acceleration_x", "acceleration_y", "acceleration_z", "gyroscope_x", "gyroscope_y", "gyroscope_z"]
+        self.df = pd.DataFrame(columns=self.columns)
 
-        # Create an MQTT client and set the message callback
-        self.mqtt_client = mqtt.Client("FastAPIMQTTListener")
+        self.mqtt_client = mqtt.Client("FastAPIMQTTListener1", clean_session=True)
         self.mqtt_client.on_message = self.on_message
         self.mqtt_client.connect(self.broker_address)
         self.mqtt_client.subscribe(self.mqtt_topic)
-
-        # Start the MQTT loop
         self.mqtt_client.loop_start()
+    
+    def unsubscribe_mqtt(self):
+        self.mqtt_client.unsubscribe(self.mqtt_topic)
+        self.mqtt_client.disconnect()
 
     def on_message(self, client, userdata, message):
-        self.mqtt_data = message.payload.decode()
+        self.mqtt_data = message.payload.decode()    
+        mqtt_data = json.loads(self.mqtt_data)
+        print(mqtt_data)
+        # Add timestamp to the data
+        mqtt_data["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        new_row = pd.DataFrame({
+            "timestamp": [mqtt_data["timestamp"]],
+            "acceleration_x": [mqtt_data["acceleration_x"]],
+            "acceleration_y": [mqtt_data["acceleration_y"]],
+            "acceleration_z": [mqtt_data["acceleration_z"]],
+            "gyroscope_x": [mqtt_data["gyroscope_x"]],
+            "gyroscope_y": [mqtt_data["gyroscope_y"]],
+            "gyroscope_z": [mqtt_data["gyroscope_z"]]
+        })
 
-mqtt_handler = MQTTHandler("broker.hivemq.com", "EEN210")
+        # Append the new row to the existing DataFrame
+        self.df = pd.concat([self.df, new_row], ignore_index=True)
+        # Save the DataFrame to a CSV file periodically
+        if len(self.df["timestamp"]) % 100 == 0:  # Save every 1000 entries (adjust as needed)
+            self.df['timestamp'] = self.df['timestamp'].astype(str)
+            self.save_to_csv()
+
+
+    def save_to_csv(self):
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        file_path = f"fall_data_{timestamp}.csv"
+        self.df.to_csv(file_path, index=False, sep=';')
+        print(f"DataFrame saved to {file_path}")
+
+class CloseEvent:
+    def __init__(self, mqtt_handler):
+        self.mqtt_handler = mqtt_handler
+
+    async def __call__(self, websocket, _):
+        self.mqtt_handler.unsubscribe_mqtt()
+
+mqtt_handler = MQTTHandler("broker.hivemq.com", "ddwddwdd")
+# mqtt_handler = MQTTHandler("broker.emqx.io", "ddwddwdd")
 
 @app.get("/")
-def read_root():
-    return {"message": "FastAPI is running"}
+async def get():
+    return HTMLResponse(html)
 
-# Without websockets, you would need to refresh the page each time
-@app.get("/mqtt_data")
-async def get_mqtt_data(request: Request):
-    mqtt_data = mqtt_handler.mqtt_data
-    if mqtt_data is not None:
-        return templates.TemplateResponse("mqtt_data.html", {"request": request, "mqtt_data": mqtt_data})
-    else:
-        return templates.TemplateResponse("mqtt_data.html", {"request": request, "mqtt_data": "No MQTT data received yet"})
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        mqtt_data = mqtt_handler.mqtt_data
+        if mqtt_data is not None:
+            print(mqtt_data)
+            # if mqtt_data.get("acceleration_x", 0) > 0.1:
+            #     mqtt_data = "ALERT!"
+            
+            await websocket.send_text(json.dumps(mqtt_data))
+        # close_event = await websocket.receive_text()
+        # if close_event == "close":
+        #     print("CLose")
+        #     await websocket.close()
+        #     break
+        # await asyncio.sleep(0.002)
+    
+
+      
 
 if __name__ == "__main__":
     import uvicorn
