570d260fca0493df66265822dce0f9aa9d17b8ef
PhDSeth <90708459+PhDSeth@users.noreply.github.com>
2024-01-15 17:39:27 +0100

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,72 +1,138 @@
-#include <Wire.h>
-#include <MPU6050.h>
-#include <WiFi.h>
-#include <WebSocketsClient.h>
-#include <Adafruit_BusIO_Register.h>
+import os
+import json
+from datetime import datetime
 
-// Public internet, allow port in firewall
-// Replace with your network credentials
-const char *ssid = "your WiFi SSID";
-const char *password = "your Passoword";
+import pandas as pd
+import uvicorn
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+from fastapi import WebSocketDisconnect
+from starlette.middleware.cors import CORSMiddleware
 
-// Replace with your WebSocket server address
-const char *webSocketServer = "your WiFi IP Address";
+app = FastAPI()
+# Enable CORS for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-const int webSocketPort = 8000;
-const char *webSocketPath = "/";
-MPU6050 mpu; // Define the sensor
-WebSocketsClient client;
-// SocketIOClient socketIO;
+with open("./src/index.html", "r") as f:
+    html = f.read()
 
-bool wifiConnected = false;
 
-void setup()
-{
-  Serial.begin(9600);
-  Wire.begin();
-  mpu.initialize();
+class DataProcessor:
+    def __init__(self):
+        self.data_buffer = []
 
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
-  client.begin(webSocketServer, webSocketPort, "/ws");
-  Serial.println(client.isConnected());
-  wifiConnected = true;
-}
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        self.file_path = f"fall_data_{timestamp}.csv"
 
-void loop()
-{
-  if (wifiConnected)
-  {
-    client.loop();
+    def add_data(self, data):
+        self.data_buffer.append(data)
 
-    // Get sensor data
-    int16_t ax, ay, az;
-    int16_t gx, gy, gz;
-    mpu.getAcceleration(&ax, &ay, &az);
-    mpu.getRotation(&gx, &gy, &gz);
+    def save_to_csv(self):
+        df = pd.DataFrame.from_dict(self.data_buffer)
+        self.data_buffer = []
+        # Append the new row to the existing DataFrame
+        df.to_csv(
+            self.file_path,
+            index=False,
+            mode="a",
+            header=not os.path.exists(self.file_path),
+        )
+        # print(f"DataFrame saved to {self.file_path}")
 
-    // Convert data to a JSON string
-    String payload = "{\"acceleration_x\":" + String(ax) +
-                     ",\"acceleration_y\":" + String(ay) +
-                     ",\"acceleration_z\":" + String(az) +
-                     ",\"gyroscope_x\":" + String(gx) +
-                     ",\"gyroscope_y\":" + String(gy) +
-                     ",\"gyroscope_z\":" + String(gz) + "}";
 
-    Serial.println("Skiikar....");
-    // server address, port and URL
-    // Send data via WebSocket
-    client.sendTXT(payload);
-    client.loop();
+data_processor = DataProcessor()
 
-    delay(10); // Adjust delay as needed
-  }
-}
+
+def load_model():
+    # you should modify this function to return your model
+    model = None
+    return model
+
+
+def predict_label(model=None, data=None):
+    # you should modify this to return the label
+    if model is not None:
+        label = model(data)
+        return label
+    return 0
+
+
+class WebSocketManager:
+    def __init__(self):
+        self.active_connections = set()
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.add(websocket)
+        print("WebSocket connected")
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+        print("WebSocket disconnected")
+
+    async def broadcast_message(self, message: str):
+        for connection in self.active_connections:
+            try:
+                await connection.send_text(message)
+            except WebSocketDisconnect:
+                # Handle disconnect if needed
+                self.disconnect(connection)
+
+
+websocket_manager = WebSocketManager()
+model = load_model()
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket_manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print("hi")
+
+            # Broadcast the incoming data to all connected clients
+            json_data = json.loads(data)
+
+            # use raw_data for prediction
+            raw_data = list(json_data.values())
+
+            # Add time stamp to the last received data
+            json_data["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            data_processor.add_data(json_data)
+            # this line save the recent 100 samples to the CSV file. you can change 100 if you want.
+            if len(data_processor.data_buffer) >= 100:
+                data_processor.save_to_csv()
+
+            """  
+            In this line we use the model to predict the labels.
+            Right now it only return 0.
+            You need to modify the predict_label function to return the true label
+            """
+            label = predict_label(model, raw_data)
+            json_data["label"] = label
+
+            # print the last data in the terminal
+            print(json_data)
+
+            # broadcast the last data to webpage
+            await websocket_manager.broadcast_message(json.dumps(json_data))
+
+    except WebSocketDisconnect:
+        websocket_manager.disconnect(websocket)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
